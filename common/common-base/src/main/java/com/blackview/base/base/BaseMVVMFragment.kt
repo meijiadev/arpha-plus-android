d291cb44688b9d073fa59ca9aa9df47c645851db
meijia <1050013833@qq.com>
2022-06-30 11:10:58 +0800

    1、将BaseMvvMFragment中获取泛型中的viewModel对象实现方法由子类实现改为在底层Base实现

--- common/common-base/src/main/java/com/blackview/base/base/BaseMVVMFragment.kt
+++ common/common-base/src/main/java/com/blackview/base/base/BaseMVVMFragment.kt
@@ -9,6 +9,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.blankj.utilcode.util.ToastUtils
 
 abstract class BaseMVVMFragment<V : ViewDataBinding, VM : BaseViewModel> : Fragment(),
@@ -19,6 +21,8 @@ abstract class BaseMVVMFragment<V : ViewDataBinding, VM : BaseViewModel> : Fragm
     var viewModelId = 0
 
     private var progressDialog: ProgressDialog? = null
+
+    private var mFragmentProvider: ViewModelProvider?=null
 
 
     override fun onCreateView(
@@ -32,7 +36,7 @@ abstract class BaseMVVMFragment<V : ViewDataBinding, VM : BaseViewModel> : Fragm
             container,
             false
         )
-        viewModel = createViewModel(this)
+        viewModel = createViewModel()
         //XML的viewModel的绑定
         viewModelId = initVariableId()
         binding.setVariable(viewModelId, viewModel)
@@ -43,12 +47,21 @@ abstract class BaseMVVMFragment<V : ViewDataBinding, VM : BaseViewModel> : Fragm
     abstract fun initLayoutId(savedInstanceState: Bundle?): Int
 
     /**
-     * 创建ViewModel 需子类必须实现
-     *
-     * @param fragment
-     * @return VM
+     * 创建属于当前fragment的viewModel对象
      */
-    abstract fun createViewModel(fragment: Fragment): VM
+    private fun createViewModel():VM{
+        return ViewModelProvider(this).get(getVmClazz(this))
+    }
+
+    /**
+     * 获取作用域为该Fragment的ViewModel
+     */
+    protected fun <T: ViewModel> getFragmentViewModel(modelClass:Class<T>):T?{
+        if (mFragmentProvider==null){
+            mFragmentProvider= ViewModelProvider(this)
+        }
+        return mFragmentProvider?.get(modelClass)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
